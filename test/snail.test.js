d6861715ed11483d55775473afde6bde6432e607
Elena <elenamarco96@gmail.com>
2020-07-28 14:42:46 +0200

    snail kata

---
+++ test/snail.test.js
@@ -0,0 +1,15 @@
+const snail = require('../lib/snail.js')
+
+describe("Snail function", ()=>{
+    it("works for simple case", ()=>{
+        array1 = [[1,2,3],[4,5,6],[7,8,9]]
+        array2 = snail(array1)
+        expect(array2).toEqual([1,2,3,6,9,8,7,4,5])
+    })
+    // it("works for arrays that are not square", ()=>{
+    //     array1=[[1,2,3,4],[4,5,6,4],[7,8,9,4]]
+    //     array2 = snail(array1)
+    //     expect(array2).toEqual([1,2,3,4,4,4,9,8,7,4,5,6])
+    // })
+
+})
